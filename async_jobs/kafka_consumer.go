58717a030c97b41bb57353f27d96e328f328e8fe
velann21 <velan@reynencourt.com>
2020-05-20 22:06:20 +0200

    Refactored the Buildconsumer func

--- async_jobs/kafka_consumer.go
+++ async_jobs/kafka_consumer.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"reflect"
 	"syscall"
 )
 
@@ -60,12 +59,53 @@ func (kf *Kafka) KafkaConsumerConfig(cc *ConsumerConfig) *Kafka {
 	return kf
 }
 
-func (kf *Kafka) Consumer() *Kafka {
-	c, err := kafka.NewConsumer(kf.ConfigMap)
+func (kf *Kafka) Consumer(sigchan chan os.Signal, cdc chan []byte) *Kafka {
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"broker.address.family": "v4",
+		"group.id":              "crawler_data_grp",
+		"session.timeout.ms":    6000,
+		"enable.partition.eof": true,
+		"auto.offset.reset":     "earliest"})
 	if err != nil {
 		fmt.Print(err)
 		log.Fatal("Error Occured")
 	}
+	err = c.SubscribeTopics([]string{"index-data"}, nil)
+	if err != nil {
+		log.Fatal("")
+	}
+
+	kf.ConsumerController = true
+	run := kf.ConsumerController
+
+	for run == true {
+		select {
+		case sig := <-sigchan:
+			fmt.Printf("Caught signal %v: terminating\n", sig)
+			run = false
+		default:
+			ev := c.Poll(100)
+			switch e := ev.(type) {
+			case *kafka.Message:
+				cdc <- e.Value
+			case *kafka.PartitionEOF:
+				fmt.Println("Partion is EOF")
+			case *kafka.Error:
+				// Errors should generally be considered
+				// informational, the client will try to
+				// automatically recover.
+				// But in this example we choose to terminate
+				// the application if all brokers are down.
+				fmt.Print("Error occured in kafka.Error")
+				fmt.Fprintf(os.Stderr, "%% Error: %v: %v\n", e.Code(), e)
+				if e.Code() == kafka.ErrAllBrokersDown {
+					run = false
+				}
+			}
+		}
+	}
+
 	kf.ConsumerObj = c
 	return kf
 }
@@ -81,10 +121,7 @@ func (kf *Kafka) AddSubscribers(topics []string) *Kafka {
 func (kf *Kafka) BuildConsumer(sigchan chan os.Signal, cdc chan []byte) {
 	kf.ConsumerController = true
 	run := kf.ConsumerController
-	rv := reflect.ValueOf(cdc)
-	if rk := rv.Kind(); rk != reflect.Chan {
-		panic("expecting type: 'chan ...'  instead got: " + rk.String())
-	}
+
 	for run == true {
 		select {
 		case sig := <-sigchan:
